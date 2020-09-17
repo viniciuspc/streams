cc4ad0c7e6627c323a3d819b613967464f3d3cde
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-17 17:50:04 +0100

    319. Creating a Stream with REST Conventions

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -24,9 +24,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
   render() {
     return (
       <form
@@ -59,7 +59,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
