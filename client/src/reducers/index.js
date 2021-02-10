a30d3c12357552f4be3c4fda8678f89ff30a4ef9
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2021-02-10 11:05:53 +0000

    Finish stream list, starting programatic navigation

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer"
 
 export default combineReducers({
   auth: authReducer,
   // The key is required to be form
   form: formReducer,
+  streams: streamReducer
 });
