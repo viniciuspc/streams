a7bb9ca5c5c0ff060db2c1d1d2c604732943b94d
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2021-04-07 17:59:57 +0100

    372. Hiding a Modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import history from '../history';
 
 const Modal = props => {
   // Creating a portal
@@ -7,9 +8,16 @@ const Modal = props => {
   // Second argument: Element to mount the portal on, usualy it is specified at
   // public/index.html
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        Modal content
+    <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
+      <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          Are you sure you want delete the stream?
+        </div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
       </div>
     </div>,
     document.querySelector('#modal')
