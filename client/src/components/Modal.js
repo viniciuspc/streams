de50b4c12685d09a9c302dbb406133d34636ef4d
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2021-04-08 18:13:25 +0100

    374. React Fragments to 379. Deleting a Stream

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import history from '../history';
+
 
 const Modal = props => {
   // Creating a portal
@@ -8,7 +8,7 @@ const Modal = props => {
   // Second argument: Element to mount the portal on, usualy it is specified at
   // public/index.html
   return ReactDOM.createPortal(
-    <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
         <div className="header">{props.title}</div>
         <div className="content">
